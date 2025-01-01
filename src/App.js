a70c4c841430e43d7bd17957d10af92c195ea57d
Tharindu Sandeepa <katharindusandeepa@gmail.com>
2025-01-01 19:55:35 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BookDetail from './pages/BookDetailsPage';
 import HomePage from './pages/HomePage';
-import AppBar from './components/AppBar'; // Import AppBar component
+import AppBar from './components/AppBar'; 
 import Footer from './components/Footer';
 import ShowBookListPage from './pages/ShowBookListPage';
 
 const App = () => {
   return (
-    <Router> {/* Added BrowserRouter component */}
-      <AppBar /> {/* Added AppBar component globally */}
+    <Router>
+      <AppBar /> 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/books/:id" element={<BookDetail />} />
         <Route path="/show-book-list" element={<ShowBookListPage />} />
       </Routes>
-      <Footer /> {/* Added Footer component globally */}
+      <Footer /> 
     </Router>
   );
 };
